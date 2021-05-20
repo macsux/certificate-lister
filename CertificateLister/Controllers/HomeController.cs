9a1236b8b6336d117bf061450ea7a9a27f389829
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-05-20 18:48:20 -0400

    better rendering

--- CertificateLister/Controllers/HomeController.cs
+++ CertificateLister/Controllers/HomeController.cs
@@ -9,47 +9,53 @@ namespace CertificateLister.Controllers
 {
     public class HomeController : Controller
     {
-       
 
-        public IEnumerable<CertStore> Index()
+     
+        public Dictionary<string, Dictionary<string, CertInfo[]>> Index()
         {
             var locations = Enum.GetValues<StoreLocation>();
             var names = Enum.GetValues<StoreName>();
             var combos = from location in locations.DefaultIfEmpty()
                 from name in names.DefaultIfEmpty()
                 select new {location, name};
-
             
-            return combos.Select(combo =>
-            {
-                using var store = new X509Store(combo.name, combo.location, OpenFlags.ReadOnly);
-                var certs = store.Certificates
-                    .Cast<X509Certificate2>()
-                    .Select(cert =>
+            return locations
+                .Select(location =>
                     {
-                        var chain = new X509Chain();
-                        chain.Build(cert);
-
-                        CertInfo certInfo = null;
-                        for (int i = chain.ChainElements.Count - 1; i >= 0; i--)
+                        return (location, stores: names.Select(storeName =>
                         {
-                            var certElement = chain.ChainElements[i];
-                            certInfo = new CertInfo(
-                                certElement.Certificate.FriendlyName,
-                                certElement.Certificate.Subject,
-                                certElement.Certificate.Thumbprint,
-                                certInfo,
-                                certElement.Certificate.Verify(),
-                                certElement.ChainElementStatus.Select(x => x.StatusInformation.ToString()).ToArray());
-                        }
-                   
-                        chain.Reset();
-                        return certInfo;
+                            using var store = new X509Store(storeName, location, OpenFlags.ReadOnly);
+                            var certs = store.Certificates
+                                .Cast<X509Certificate2>()
+                                .Select(cert =>
+                                {
+                                    var chain = new X509Chain();
+                                    chain.Build(cert);
+
+                                    CertInfo certInfo = null;
+                                    for (int i = chain.ChainElements.Count - 1; i >= 0; i--)
+                                    {
+                                        var certElement = chain.ChainElements[i];
+                                        certInfo = new CertInfo(
+                                            certElement.Certificate.FriendlyName,
+                                            certElement.Certificate.Subject,
+                                            certElement.Certificate.Thumbprint,
+                                            certElement.Certificate.NotBefore,
+                                            certElement.Certificate.NotAfter,
+                                            certInfo,
+                                            certElement.Certificate.Verify(),
+                                            certElement.ChainElementStatus.Select(x => x.StatusInformation.ToString()).ToArray());
+                                    }
+
+                                    chain.Reset();
+                                    return certInfo;
+                                })
+                                .ToArray();
+                            return (storeName, certs);
+                            // return new CertStore(storeName.ToString(), certs);
+                        }).ToDictionary(x => x.storeName.ToString(), x => x.certs));
                     })
-                    .ToArray();
-                
-                return new CertStore(combo.name.ToString(), combo.location.ToString(), certs);
-            });
+                .ToDictionary(x => x.location.ToString(), x => x.stores);
             
             
         }
@@ -57,6 +63,7 @@ namespace CertificateLister.Controllers
         
     }
 
-    public record CertStore(string StoreName, string StoreLocation, CertInfo[] Certificates);
-    public record CertInfo(string Name, string Subject, string Thumbprint, CertInfo Issuer, bool IsValid, string[] ValidationMessages);
+    public record CertLocation(string Location, CertStore[] Stores);
+    public record CertStore(string StoreName, CertInfo[] Certificates);
+    public record CertInfo(string Name, string Subject, string Thumbprint, DateTime validFrom, DateTime validTo, CertInfo Issuer, bool IsValid, string[] ValidationMessages);
 }
